051ec6a680d21aa59aba98f9efccd1506dc382bf
xiaohubai <xiaohubai@outlook.com>
2023-04-28 20:18:08 +0800

    v0.2

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    base: env.VITE_BASE_NAME,
-    
+    base: '/vue-ts-layout/',
+
     resolve: {
       alias: {
         '@': __dirname + '/src',
