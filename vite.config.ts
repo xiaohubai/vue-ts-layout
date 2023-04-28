148a45adb1fadf1a29ae52a7377da2e31a95fa9e
xiaohubai <xiaohubai@outlook.com>
2023-04-28 20:22:11 +0800

    v0.2

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
     base: '/vue-ts-layout/',
-
+    root:'./',
     resolve: {
       alias: {
         '@': __dirname + '/src',
