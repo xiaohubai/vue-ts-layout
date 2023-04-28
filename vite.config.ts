b0a9566e602fb008aab16d3cf1e27d1d9102252a
xiaohubai <xiaohubai@outlook.com>
2023-04-28 19:32:13 +0800

    fix:修改base参数

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
+    base: env.VITE_BASE_NAME,
+    
     resolve: {
       alias: {
         '@': __dirname + '/src',
