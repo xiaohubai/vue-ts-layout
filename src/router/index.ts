905c8915b27797d667988222159b455875e131d9
xiaohubai <xiaohubai@outlook.com>
2023-04-29 01:06:00 +0800

    fix:修改router为hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes: any = [
   {
@@ -17,7 +17,7 @@ const routes: any = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHashHistory(),
   routes
 })
 
